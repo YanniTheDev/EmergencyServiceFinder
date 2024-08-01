ce987297bd6af5b05bbda7bd906335beedc8fea1
Yanxuan Weng <23468@nayland.school.nz>
2024-08-02 10:39:15 +1200

    Doing search area

---
+++ interactive-tourist-guide/src/Components/SearchArea.jsx
@@ -0,0 +1,12 @@
+import "../ComponentCSS/SearchArea.css";
+import "../Reusables.css"
+
+export const SearchArea = () => {
+    return (
+        <div className="search-area flex-dir-col flex-c-c">
+            <h1>Enter Your Address</h1>
+
+            <input type="text" />
+        </div>
+    );
+}

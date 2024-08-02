9e49cdd75a37783a1cd0202b1cf3ac3e2521fe72
YanniTheDev <yannithedev@gmail.com>
2024-08-02 19:24:25 +1200

    Still doing search area

--- interactive-tourist-guide/src/Components/SearchArea.jsx
+++ interactive-tourist-guide/src/Components/SearchArea.jsx
@@ -4,9 +4,21 @@ import "../Reusables.css"
 export const SearchArea = () => {
     return (
         <div className="search-area flex-dir-col flex-c-c">
-            <h1>Enter Your Address</h1>
+            <div className="address-input flex-dir-col flex-c-c">
+                <h1>Enter Your Address</h1>
 
-            <input type="text" />
+                <input type="text" />
+            </div>
+
+            <div className="distance-input flex-dir-col flex-c-c">
+                <h1>Maximum Travel Distance</h1>
+
+                <input type="number" />
+            </div>
+
+            <button>
+                Find Restaurants
+            </button>
         </div>
     );
 }

51966a56b45a499c5c41fa0cd63716be9acea0ea
YanniTheDev <yannithedev@gmail.com>
2024-08-02 20:33:17 +1200

    Done

--- interactive-tourist-guide/src/App.jsx
+++ interactive-tourist-guide/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+
 import './App.css';
+import "./Reusables.css";
 
 import { SearchArea } from './Components/SearchArea.jsx';
+import { Map } from "./Components/Map.jsx";
 
 function App() {
 
   return (
     <>
-      <SearchArea />
+      <div className="flex-s-s flex-dir-row">
+        <SearchArea />
+        <Map />
+      </div>
     </>
   );
   
---
+++ interactive-tourist-guide/src/Components/Map.jsx
@@ -0,0 +1,9 @@
+import "../ComponentCSS/MapArea.css";
+
+export const Map = () => {
+    return (
+        <div className="map-area flex-c-c flex-dir-column">
+            <h1><b>MAP STUFF HERE</b></h1>
+        </div>
+    );
+}

829eee1fd065af457542fcfeb3bb7b67abedb32b
Yanxuan Weng <23468@nayland.school.nz>
2024-08-08 12:35:34 +1200

    Yeah

--- interactive-tourist-guide/src/Components/SearchArea.jsx
+++ interactive-tourist-guide/src/Components/SearchArea.jsx
@@ -4,16 +4,16 @@ import "../Reusables.css"
 export const SearchArea = () => {
     return (
         <div className="search-area flex-dir-col flex-c-c">
-            <div className="address-input flex-dir-col flex-c-c">
+            <div className="address-container flex-dir-col flex-c-c">
                 <h1>Enter Your Address</h1>
 
-                <input type="text" />
+                <input type="text" placeholder="e.g. 3 Jo Road, Nelson, New Zealand"/>
             </div>
 
-            <div className="distance-input flex-dir-col flex-c-c">
+            <div className="distance-container flex-dir-col flex-c-c">
                 <h1>Maximum Travel Distance</h1>
 
-                <input type="number" />
+                <input type="number" placeholder="e.g. 35"/>
             </div>
 
             <button>

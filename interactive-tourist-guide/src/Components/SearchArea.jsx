91c5660530319450e336b3e4db47345f64746db6
YanniTheDev <yannithedev@gmail.com>
2024-08-09 21:32:38 +1200

    I hate this assignment

--- interactive-tourist-guide/src/Components/SearchArea.jsx
+++ interactive-tourist-guide/src/Components/SearchArea.jsx
@@ -8,25 +8,52 @@ import "../Reusables.css"
 export const SearchArea = () => {
 
     const [address, setAddress] = useState("");
-
     const getAddress = (event) => {
         setAddress(event.target.value);
     }
-
-    const [addressInfo, setAddressInfo] = useState(0);
+    
+    const [maxTravelDistance, setMaxTravelDistance] = useState(0);
+    const getMaxTravelDistance = (event) => {
+        setMaxTravelDistance(event.target.value);
+    }
 
     const apiKey = import.meta.env.VITE_HERE_API_KEY;
 
     const geoCodeAddress = () => {
         let url = `https://geocode.search.hereapi.com/v1/geocode?q=${address}&apiKey=${apiKey}`
 
+        //API REQUEST!
         Axios.get(url).then((response) => {
 
             //Weird React thing going on. Why make new variable am i right???
-            const apiResponse = response;
-            setAddressInfo(apiResponse);
+            let apiResponse = response;
+            let apiResponseCoords = response.data.items[0].position;
 
             console.log(apiResponse);
+            console.log(apiResponseCoords);
+
+            findPetrolStations(apiResponseCoords);
+
+        }).catch((error) => {
+            console.error(error);
+        })
+    }
+
+    const findPetrolStations = (coords) => {
+        let url = `https://discover.search.hereapi.com/v1/discover?at=${coords.lat},${coords.lng}&q=petrol+station&apiKey=${apiKey}`
+    
+        //API REQUEST!!
+        Axios.get(url).then((response) => {
+
+            //Weird React thing going on. Why make new variable am i right???
+            let petrolStations = response.data.items;
+
+            let validPetrolStations = petrolStations.filter((element) => {
+                return element.distance <= maxTravelDistance;
+            })
+
+            console.log(petrolStations);
+            console.log(validPetrolStations);
 
         }).catch((error) => {
             console.error(error);
@@ -44,7 +71,7 @@ export const SearchArea = () => {
             <div className="distance-container flex-dir-col flex-c-c">
                 <h1>Maximum Travel Distance</h1>
 
-                <input type="number" placeholder="e.g. 35"/>
+                <input type="number" placeholder="e.g. 35" onChange={getMaxTravelDistance}/>
             </div>
 
             <button onClick={geoCodeAddress}>

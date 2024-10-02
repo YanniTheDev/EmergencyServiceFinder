c5e91ab87da415b1c7efffaf9cff0bb2c3680836
YanniTheDev <yannithedev@gmail.com>
2024-10-02 20:26:26 +1300

    finished refactoring

--- interactive-tourist-guide/src/App.jsx
+++ interactive-tourist-guide/src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css';
 import "./Reusables.css";
 
-import { SearchArea } from './Components/SearchArea.jsx';
-import { Map } from "./Components/Map.jsx";
-
 import { AppContent } from "./Components/Content.jsx";
 
-import { createContext, useContext, useState } from 'react';
+import { createContext } from 'react';
 
 export const AppContext = createContext();
 
@@ -14,27 +11,9 @@ function App() {
 
   const apiKey = import.meta.env.VITE_HERE_API_KEY;
 
-  const [validRestaurants, setValidRestaurants] = useState([]);
-  const [displayMarker, setDisplayMarker] = useState(0);
-  const [userCoord, setUserCoord] = useState({lat: 64.144, lng: -21.94});
-
   return (
     <>
       <div className="app flex-s-s flex-dir-row">
-
-        {/* Components
-        {/* Using props! */}
-        {/*}
-        <SearchArea apiKey={apiKey}/> 
-        <Map apiKey={apiKey} /> */}
-
-        {/* <AppContext.Provider value={{ restaurants: [validRestaurants, setValidRestaurants], marker: [displayMarker, setDisplayMarker], user: [userCoord, setUserCoord]}}>
-          {/* Components */}
-          {/* Using props! */}
-        {/*
-          <SearchArea apiKey={apiKey}/> 
-          <Map apiKey={apiKey} />
-        </AppContext.Provider> */}
 
         <AppContent apiKey={apiKey}/>
 

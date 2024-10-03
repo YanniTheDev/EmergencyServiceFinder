ebc847f3ed03cc7cb59b988d174fc86ee42ad7ce
YanniTheDev <yannithedev@gmail.com>
2024-10-03 20:21:01 +1300

    doing routes

--- interactive-tourist-guide/src/Components/Map.jsx
+++ interactive-tourist-guide/src/Components/Map.jsx
@@ -51,13 +51,26 @@ export const Map = forwardRef(({ apiKey, userCoord }, ref) => {
     //Exposes the displayMap function to the parent component
     useImperativeHandle(ref, () => {
         return {
-            addRestaurantMarkers: addRestaurantMarkers, 
+            addRestaurantMarkers: addRestaurantMarkers,
+            handleRoutes: handleRoutes
         }
     });
 
     const addRestaurantMarkers = (inDistanceRestaurants) => {
 
         map.current.removeObjects(map.current.getObjects());
+
+        // Define a variable holding SVG mark-up that defines an icon image:
+        const svgMarkup = '<svg width="50" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="50" ' +
+        'height="22" /><text x="25" y="17" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">Start</text></svg>';
+        const icon = new H.map.Icon(svgMarkup);
+        const homeMarker = new H.map.Marker(userCoord, { icon: icon});
+
+        map.current.addObject(homeMarker);
         
         inDistanceRestaurants.forEach(element => {
             
@@ -68,6 +81,10 @@ export const Map = forwardRef(({ apiKey, userCoord }, ref) => {
                 console.log(error)
             }
         });
+
+    }
+    
+    const handleRoutes = () => {
 
     }
 

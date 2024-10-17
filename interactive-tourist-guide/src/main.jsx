4f2b4e3999f356766e6e9864515438f280519bc5
Yanxuan Weng <23468@nayland.school.nz>
2024-10-18 09:59:13 +1300

    added comments

--- interactive-tourist-guide/src/main.jsx
+++ interactive-tourist-guide/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Enables strict mode so that JSX is more strict with its syntax
   <React.StrictMode>
     <App />
   </React.StrictMode>,
